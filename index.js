1da0f8c8c5ffb79fd6860aa4908cce3613d4a086
Ray C <rayc2045@gmail.com>
2021-07-14 22:17:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,30 @@ const chalk = require('chalk');
 const clipboardy = require('clipboardy');
 const createPassword = require('./utils/createPassword');
 const savePassword = require('./utils/savePassword');
+const log = console.log;
+const fileToSave = 'passwords.txt';
 
 // node index -h
-program.version('1.0.0').description('Node.js Password Generator');
-
 program
+  .version('1.0.0')
+  .description('Node.js Password Generator')
   .option('-l, --length <numbers>', 'length of password', '8') // default length: 8
   .option('-nn, --no-numbers', 'remove numbers')
   .option('-ns, --no-symbols', 'remove symbols')
-  .option('-s, --save', 'save password to password.txt')
+  .option('-s, --save', `save password to ${fileToSave}`)
   .parse();
 
 const { length, numbers, symbols, save } = program.opts();
 
-const password = createPassword(length, numbers, symbols, save);
-console.log(chalk.blue('Password: ') + chalk.bold(password));
+// Generate password
+const password = createPassword(length, numbers, symbols);
 
+// Copies to clipboard
 clipboardy.writeSync(password);
-console.log(chalk.yellow('Password copies to clipboard'));
 
-if (save) savePassword(password);
+// Save password to file
+if (save) savePassword(password, fileToSave);
+
+// Output generated password
+log(chalk.blue('Generated Password: ') + chalk.bold(password));
+log(chalk.yellow('Password copies to clipboard'));
