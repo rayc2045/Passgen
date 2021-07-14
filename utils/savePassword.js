042eb2e7a605e189a46668376e097cf8105d399c
Ray C <rayc2045@gmail.com>
2021-07-14 22:16:52 +0800

    Update savePassword.js

--- utils/savePassword.js
+++ utils/savePassword.js
@@ -2,15 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const chalk = require('chalk');
+const log = console.log;
 
-const savePassword = password => {
-  fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 666, (e, id) => {
+function savePassword(password, fileToSave) {
+  fs.open(path.join(__dirname, '../', fileToSave), 'a', 666, (e, id) => {
     fs.write(id, password + os.EOL, null, 'utf-8', () => {
-      fs.close(id, () => {
-        console.log(chalk.green('Password saved to passwords.txt'));
-      });
+      fs.close(id, () => log(chalk.green('Password saved to passwords.txt')));
     });
   });
-};
+}
 
 module.exports = savePassword;
